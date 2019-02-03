1c659cfd797f938dd5e1f9537e6f633b05ed74cf
Wilgens M <wilgens21@gmail.com>
2019-02-03 18:33:22 -0500

    Create SimpleHammer.cpp file
    Created the implementation file the simple hammer class, which deals 25 hitpoints and ignores the armor if the armor's points are less than 30

---
+++ SimpleHammer.cpp
@@ -0,0 +1,20 @@
+/*
+ * File:   SimpleHammer.h
+ * Author: Wilgens <wilgens21@gmail.com>
+ *
+ * Created on February 3, 2019, 6:00 PM
+ */
+
+#include "SimpleHammer.h"
+
+double CommonSpear::hit(double armor)
+{
+  double damage = hitPoints - armor;
+
+  if(armor < 30)
+    return hitPoints;
+  if else(damage < 0)
+    return 0;
+
+    return damage;
+}
