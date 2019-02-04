694c7972c7b392afafa79118319671392e1dc1b7
Wilgens M <wilgens21@gmail.com>
2019-02-03 20:12:29 -0500

    Change behavior of needle in PoisonNeedle.cpp
    Did not read instructions carefully, so the needle's hitpoint needed to be chang
    ed to 1 and when it hits armor less than 5 it will ignore armor and deal 99 damage.

--- PoisonNeedle.cpp
+++ PoisonNeedle.cpp
@@ -3,16 +3,23 @@
  * Author: Wilgens <wilgens21@gmail.com>
  *
  * Created on February 3, 2019, 7:37 PM
+
+ Change needle behavior
+
+Did not read instructions carefully, so the needle's hitpoint needed to be chang
+ed to 1 and when it hits armor less than 5 it will ignore armor and deal 99 damage.
  */
 
 #include "PoisonNeedle.h"
 
 double PoisonNeedle::hit(double armor)
 {
+  double damage = hitPoints - armor;
 
-  if(armor == 0)
+  if(armor < 5)
     return 99.0;
-
-
+  if else(damage < 0)
     return 0;
+
+    return damage;
 }
