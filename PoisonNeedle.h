b2872fd56b6803ebe2c41f410191de0ed10ff854
Wilgens M <wilgens21@gmail.com>
2019-02-03 19:44:08 -0500

    Create PoisonNeedle.h
    Created the header file for the poison needle class that has a hit point of 0 unless the armor points are at 0 then it will deal 75 hitpoints instead.

---
+++ PoisonNeedle.h
@@ -0,0 +1,32 @@
+/*
+ * File:   PoisonNeedle.h
+ * Author: Wilgens <wilgens21@gmail.com>
+ *
+ * Created on February 3, 2019, 7:37 PM
+ */
+
+#include <string>
+#include "Weapon.h"
+
+#ifndef POISONNEEDLE_H
+#define POISONNEEDLE_H
+
+/**
+ * Defines the behavior of a Poison needle (hitpoint = 0, If the armor is 0, the needle will instead deal 75 hitpoints.
+)
+ */
+
+ class PoisonNeedle: public Weapon
+ {
+    public:
+      PoisonNeedle() : Weapon("Poison Needle",0) //Calls Weapon(name, hitpoints) constructor with values Poison Needle and 0
+      {
+      }
+
+      virtual ~PoisonNeedle() {};
+
+      virtual double hit(double armor);
+
+ };
+
+ #endif /* POISONNEEDLE_H */
