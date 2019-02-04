85b305beba89517c4274755a2f861455ce3694a5
Wilgens M <wilgens21@gmail.com>
2019-02-03 19:57:03 -0500

    Create PoisonNeedle.cpp
    Created the implementation file for the poison needle class that deals 0 hipoints unless the armor points is 0 then it will deal 99 hitpoints instead.

---
+++ PoisonNeedle.cpp
@@ -0,0 +1,18 @@
+/*
+ * File:   PoisonNeedle.cpp
+ * Author: Wilgens <wilgens21@gmail.com>
+ *
+ * Created on February 3, 2019, 7:37 PM
+ */
+
+#include "PoisonNeedle.h"
+
+double PoisonNeedle::hit(double armor)
+{
+
+  if(armor == 0)
+    return 99.0;
+
+
+    return 0;
+}
